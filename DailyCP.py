04cb3066992d11ed3d84dab4501e6960cc0369e7
LENOVO <1005925021@qq.com>
2020-07-01 23:16:31 +0800

    save

--- DailyCP.py
+++ DailyCP.py
@@ -227,13 +227,5 @@ if __name__ == "__main__":
         exit()
     app = DailyCP(sys.argv[1])
     if not app.login(sys.argv[2], sys.argv[3]):exit()
+    print('正确登录，正在填写表格')
     app.autoComplete(sys.argv[4],sys.argv[5])
-
-# Author:HuangXu,FengXinYang,ZhouYuYang.
-# By:AUST HACKER
-
-#2020/5/20 重要更新：修复登录过程，移除验证码（不需要），优化代码格式，感谢giteee及时反馈。
-#2020/5/28 更改为使用自动获取学校URL的方式，更改为使用参数形式，添加另一种登录形式AuthServer的支持(已完成但未测试)。感谢柠火的反馈。
-#2020/6/1 修复BUG，发现AuthServer的登录方式每个学校都不一样。支持任意表单内容自定义（详情见输出信息和formdb/1129.json）。感谢涅灵的反馈。
-#2020/6/2 AuthServer的登录网址不再使用硬编码的方式，理论上能支持所有学校了吧？感谢涅灵的反馈。
-#2020/6/17 修复crontab使用中相对路径的问题。识别form特征。
